fc9802471710a5d8e0989c39fd6295d7b160c490
cristinajeandonato <cristinajeandonato@gmail.com>
2014-05-05 10:22:49 +0800

    added views for votes

--- src/steelrumors/links/models.py
+++ src/steelrumors/links/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Count
+
+class LinkVoteCountManager(models.Manager):
+    def get_query_set(self):
+        return super(LinkVoteCountManager, self).get_query_set().annotate(
+            votes=Count('vote')).order_by('-votes')
 
 class Link(models.Model):
     title = models.CharField("Headlines", max_length=100)
@@ -8,6 +14,9 @@ class Link(models.Model):
     rank_score = models.FloatField(default=0.0)
     url = models.URLField("URL", max_length=250, blank=True)
     description = models.TextField(blank=True)
+
+    with_votes = LinkVoteCountManager()
+    objects = models.Manager() 
 
     def __unicode__(self):
         return self.title
@@ -18,3 +27,5 @@ class Vote(models.Model):
 
     def __unicode__(self):
         return "%s voted %s" % (self.voter.username, self.link.title)
+
+
--- src/steelrumors/steelrumors/urls.py
+++ src/steelrumors/steelrumors/urls.py
@@ -16,5 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', LinkListView.as_view(), name='home'),
     url(r'^links/', include('links.urls', namespace="links"))
 )
